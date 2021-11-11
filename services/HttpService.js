80e64c122c4eafde9ee875634ec324c53fe09463
Vasyl Teliman <vasniktel@gmail.com>
2021-11-11 19:33:51 +0200

    Rename API env variable

--- services/HttpService.js
+++ services/HttpService.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { getToken, setToken } from "../utils/AuthUtils";
 
-const BASE_URL = process.env.API_BASE_URL || "http://localhost:8080/api";
+const BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:8080/api";
 
 const instance = axios.create({
     baseURL: BASE_URL,
