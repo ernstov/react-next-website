4fbe3d1176a9c0b4de45a63965a88d14b0e96901
Ernst <voroniukernst@gmail.com>
2021-11-16 12:10:27 +0100

    Update: Carousel, Animation

--- sections/Hero/AnimationLogo.jsx
+++ sections/Hero/AnimationLogo.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useRef } from "react"
 import styles from './hero.module.scss'
-import animationData from '../../components/animations/darkHorizontal.json'
+import animationData from '../../components/animations/lightHorizontal.json'
 import Lottie from 'lottie-web'
 
 const AnimationLogo = ({isVisible}) => {
